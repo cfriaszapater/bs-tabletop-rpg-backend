97d1431ec231918928e8ba7bb37fc67815bb1432
cfriaszapater <>
2019-11-20 11:07:10 +0100

    basic routes

--- routes/characters.js
+++ routes/characters.js
@@ -2,12 +2,17 @@ var express = require("express");
 var router = express.Router();
 var debug = require("debug")("bs-tabletop-rpg-backend:routers:characters");
 
-router.get("/:characterId", get);
+router.get("/", listCharacters);
+router.get("/:characterId", getCharacter);
 
 module.exports = router;
 
-const get = async function(req, res, next) {
+const getCharacter = async (req, res, next) => {
   debug("get character", req.params.characterId);
 
-  return "TODO combat";
+  return "TODO character";
+};
+
+const listCharacters = async req => {
+  return "TODO list characters";
 };
--- routes/combats.js
+++ routes/combats.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 var debug = require("debug")("bs-tabletop-rpg-backend:routers:combats");
 
-router.get("/:combatId", get);
+router.get("/:combatId", getCombat);
 
 module.exports = router;
 
-const get = async function(req, res, next) {
+const getCombat = async function(req, res, next) {
   debug("get combat", req.params.combatId);
 
   return "TODO combat";
