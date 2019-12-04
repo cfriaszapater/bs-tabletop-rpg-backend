1f8fd30d99d5f12b85b32f13026c8bfd590d89e8
cfriaszapater <>
2019-12-04 09:24:00 +0100

    combat spec - WIP

--- domain/combat.js
+++ domain/combat.js
@@ -1,11 +1,47 @@
-exports.startCombat = combat => {
-  // TODO TDD THIS:
-  // TODO Validate characters exist, etc
-  if (!combat.participants === undefined || combat.participants.length < 2) {
-    throw new Error("There should be at least 2 participants to start combat");
+function startCombat(combat) {
+  if (!enoughParticipants(combat)) {
+    throw new Error(notEnoughParticipantsError);
   }
 
-  // TODO Set character with highest Ini as turn.attacker
+  // TODO Assume characters exist in the system (they have been validated before calling startCombat)
 
-  return combat;
+  // TODO Set character with highest Ini as turn.attacker
+  const startedCombat = {
+    ...combat,
+    turn: {
+      attacker: firstToAct(combat.participants)
+    }
+  };
+
+  return startedCombat;
+}
+
+const notEnoughParticipantsError =
+  "There should be at least 2 participants to start combat";
+
+function enoughParticipants(combat) {
+  return (
+    typeof combat.participants !== "undefined" &&
+    combat.participants.length >= 2
+  );
+}
+
+function firstToAct(characters) {
+  const reducer = (highestIni, current) => {
+    if (
+      highestIni === undefined ||
+      current.characteristics.initiative.current >
+        highestIni.characteristics.initiative.current
+    ) {
+      return current;
+    } else {
+      return highestIni;
+    }
+  };
+  return characters.reduce(reducer);
+}
+
+module.exports = {
+  startCombat,
+  notEnoughParticipantsError
 };
--- domain/combat.spec.js
+++ domain/combat.spec.js
@@ -1,5 +1,48 @@
-const { startCombat } = require("./combat");
+const { startCombat, notEnoughParticipantsError } = require("./combat");
 
 describe("Combat", () => {
-  it("should set turn.attacker as highest Ini character on start combat", () => {});
+  it("given combat with participants, when start combat, then set turn.attacker as highest Ini character", () => {
+    const character1 = character("C1", 6);
+    const character2 = character("C2", 5);
+    const combat = { participants: [character1, character2] };
+
+    const startedCombat = startCombat(combat);
+
+    expect(startedCombat.turn.attacker).toEqual(character1);
+  });
+
+  it("given no participants, when start combat, then error", () => {
+    expect(() => {
+      startCombat({});
+    }).toThrow("2 participant");
+
+    expect(() => {
+      startCombat({ participants: undefined });
+    }).toThrow(notEnoughParticipantsError);
+
+    expect(() => {
+      startCombat({ participants: [] });
+    }).toThrow(notEnoughParticipantsError);
+
+    expect(() => {
+      startCombat({ participants: [character("jarl", 1)] });
+    }).toThrow(notEnoughParticipantsError);
+  });
 });
+
+// Character with only the attributes needed to decide who acts first in initiative turn
+function character(id, ini, reach, agi, int) {
+  return {
+    id: id,
+    attributes: {
+      agility: agi,
+      intelligence: int
+    },
+    characteristics: {
+      initiative: {
+        current: ini
+      },
+      reach: reach
+    }
+  };
+}
